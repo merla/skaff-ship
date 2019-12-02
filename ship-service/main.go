78f2b5456ec07e3eda2bf1ffc76dba690f7833c8
Paul Nguyen <punkupoz@gmail.com>
2019-12-02 12:11:50 +1000

    Gitlab (#2)
    * donut gitlab
    
    * add deploy stage
    
    * change helm
    
    * remove docker on deploy
    
    * remove conditional pipeline for now
    
    * helm cli
    
    * helm name
    
    * helm
    
    * helm service
    
    * more delicious
    
    * helm up
    
    * helm up
    
    * contraint logic
    
    * short sha
    
    * upgarde donut version
    
    * gitlab
    
    * gitlab
    
    * yeets
    
    * add ship ci
    
    * dac
    
    * removen need
    
    * use extends
    
    * use extends
    
    * wat
    
    * dac
    
    * dac
    
    * neeed
    
    * ship
    
    * dasd

--- ship-service/main.go
+++ ship-service/main.go
@@ -16,7 +16,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	donut, err := request("http://donut-service:8441", bytes.NewReader(signed))
+	donut, err := request("http://donut-service-prod:8441", bytes.NewReader(signed))
 	if err != nil {
 		panic(err)
 	}
@@ -27,7 +27,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Print("Shipper is ready")
+	log.Print("Shipper is ready 1")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8442", nil)
 }
